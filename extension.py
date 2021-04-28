1477d894e86f8843939edf6343d21555650b0c0b
Alexandra <aallard170@gmail.com>
2021-04-28 12:58:16 -0700

    Altered solution

--- extension.py
+++ extension.py
@@ -9,7 +9,7 @@ This file is for optional extension programs.
 def main():
     """
     Karel draws the text, "HELLO WORLD". Works in Extension world 20*20.
-    pre: Karel is facing east at (01, 01), prior to writing "HELLO WORLD".
+    pre: Karel has to be facing east at (01, 01), prior to writing "HELLO WORLD".
     post: Karel is facing east at (20, 10), after writing "HELLO WORLD".
     """
     move_to_top_line()
@@ -31,21 +31,18 @@ pre: Karel is facing north, prior to writing.
 post: Karel is facing east, two spaces past the completed letter H.
 """
 def draw_h():
-    turn_around()
-    draw_line()
-    turn_around()
+    turn_south()
+    draw_line_len5()
+    turn_north()
     for i in range(2):
         move()
-    turn_right()
-    move()
-    paint_corner(CYAN)
-    move()
-    turn_left()
-    for i in range(2):
-        move()
-    turn_around()
-    draw_line()
-    turn_left()
+    turn_east()
+    draw_line_len2()
+    turn_north()
+    draw_line_len2()
+    turn_south()
+    draw_line_len5()
+    turn_east()
     for i in range(2):
         move()
 
@@ -54,20 +51,27 @@ pre: Karel is facing east, two spaces past the previous letter.
 post: Karel is facing east, two spaces past the completed letter E.
 """
 def draw_e():
-    turn_left()
-    draw_line()
-    turn_right()
-    draw_prong()
+    turn_north()
+    draw_line_len5()
+    turn_east()
+    draw_line_len2()
+    turn_west()
     for i in range(2):
         move()
-    turn_left()
-    draw_prong()
+    turn_south()
     for i in range(2):
         move()
-    turn_left()
-    draw_prong()
-    turn_left()
-    for i in range(4):
+    turn_east()
+    draw_line_len2()
+    turn_west()
+    for i in range(2):
+        move()
+    turn_south()
+    for i in range(2):
+        move()
+    turn_east()
+    draw_line_len2()
+    for i in range(2):
         move()
 
 """ 
@@ -75,15 +79,14 @@ pre: Karel is facing east, two spaces past the previous letter.
 post: Karel is facing east, two spaces past the completed letter L.
 """
 def draw_l():
-    turn_left()
-    draw_line()
-    turn_around()
+    turn_north()
+    draw_line_len5()
+    turn_south()
     for i in range(4):
         move()
-    turn_left()
-    draw_prong()
-    turn_left()
-    for i in range(4):
+    turn_east()
+    draw_line_len2()
+    for i in range(2):
         move()
 
 """ 
@@ -91,19 +94,15 @@ pre: Karel is facing east, two spaces past the previous letter.
 post: Karel is facing east, one space past the completed letter O. 
 """
 def draw_o():
-    turn_left()
-    draw_line()
-    turn_right()
-    move()
-    paint_corner(CYAN)
-    move()
-    turn_right()
-    draw_line()
-    turn_right()
-    move()
-    paint_corner(CYAN)
-    move()
-    turn_around()
+    turn_north()
+    draw_line_len5()
+    turn_east()
+    draw_line_len2()
+    turn_south()
+    draw_line_len5()
+    turn_west()
+    draw_line_len2()
+    turn_east()
     for i in range(3):
         move()
 
@@ -112,23 +111,21 @@ pre: Karel is facing west.
 post: Karel is facing east, two spaces past the completed letter W. 
 """
 def draw_w():
-    turn_right()
-    draw_line()
-    turn_around()
+    turn_north()
+    draw_line_len5()
+    turn_south()
     for i in range(3):
         move()
-    turn_left()
+    turn_east()
+    draw_line_len2()
+    turn_south()
     move()
-    paint_corner(CYAN)
-    move()
-    turn_right()
-    move()
-    turn_around()
-    draw_line()
-    turn_around()
+    turn_north()
+    draw_line_len5()
+    turn_south()
     for i in range(4):
         move()
-    turn_left()
+    turn_east()
     for i in range(2):
         move()
 
@@ -137,32 +134,24 @@ pre: Karel is facing east, two spaces past the previous letter.
 post: Karel is facing east, two spaces past the completed letter R.
 """
 def draw_r():
-    turn_left()
-    draw_line()
-    turn_right()
+    turn_north()
+    draw_line_len5()
+    turn_east()
+    draw_line_len2()
+    turn_south()
+    draw_line_len2()
+    turn_west()
+    draw_line_len2()
+    turn_south()
+    move()
+    turn_east()
     move()
     paint_corner(CYAN)
     move()
-    turn_right()
-    paint_corner(CYAN)
+    turn_south()
     move()
     paint_corner(CYAN)
-    move()
-    paint_corner(CYAN)
-    turn_right()
-    move()
-    paint_corner(CYAN)
-    move()
-    turn_left()
-    move()
-    turn_left()
-    move()
-    paint_corner(CYAN)
-    move()
-    turn_right()
-    move()
-    paint_corner(CYAN)
-    turn_left()
+    turn_east()
     for i in range(2):
         move()
 
@@ -171,28 +160,25 @@ pre: Karel is facing east, two spaces past the previous letter.
 post: Karel is facing east, one space past the completed letter D.
 """
 def draw_d():
-    turn_left()
-    draw_line()
-    turn_right()
+    turn_north()
+    draw_line_len5()
+    turn_east()
+    draw_line_len1()
     move()
-    paint_corner(CYAN)
+    turn_south()
     move()
-    turn_right()
+    draw_line_len2()
     move()
-    for i in range(3):
-        paint_corner(CYAN)
-        move()
-    turn_right()
+    turn_west()
     move()
-    paint_corner(CYAN)
-    move()
-    turn_around()
+    draw_line_len1()
+    turn_east()
     for i in range(3):
         move()
 
 # Karel moves to 01, 20.
 def move_to_top_line():
-    turn_left()
+    turn_north()
     while front_is_clear():
         move()
 
@@ -201,40 +187,60 @@ pre: Karel is at the end of a 'line' of text, facing east.
 post: Karel is at the beginning of the next 'line', facing west.
 """
 def move_down():
-    turn_right()
+    turn_south()
     for i in range(6):
         move()
-    turn_right()
+    turn_west()
     while front_is_clear():
         move()
 
+# Karel turns north.
+def turn_north():
+    while not_facing_north():
+        turn_left()
+
+# Karel turns east.
+def turn_east():
+    while not_facing_east():
+        turn_left()
+
+# Karel turns south.
+def turn_south():
+    while not_facing_south():
+        turn_left()
+
+# Karel turns west.
+def turn_west():
+    while not_facing_west():
+        turn_left()
+
 # Karel draws a line 5 spaces long.
-def draw_line():
-    for i in range(4):
-        paint_corner(CYAN)
-        move()
+def draw_line_len5():
     paint_corner(CYAN)
+    for i in range(4):
+        move()
+        paint_corner(CYAN)
+
+# Karel draws a line 3 spaces long.
+def draw_line_len3():
+    paint_corner(CYAN)
+    for i in range(3):
+        move()
+        paint_corner(CYAN)
 
 # Karel draws a line 2 spaces long.
-def draw_prong():
-    move()
+def draw_line_len2():
     paint_corner(CYAN)
-    move()
-    paint_corner(CYAN)
-    turn_around()
     for i in range(2):
         move()
-    turn_left()
+        paint_corner(CYAN)
 
-# Karel turns right
-def turn_right():
-    for i in range(3):
-        turn_left()
-
-# Karel turns around
-def turn_around():
-    for i in range(2):
-        turn_left()
+# Karel draws a line 1 space long.
+def draw_line_len1():
+    paint_corner(CYAN)
+    for i in range(1):
+        move()
+        paint_corner(CYAN)
 
 if __name__ == "__main__":
     run_karel_program()
